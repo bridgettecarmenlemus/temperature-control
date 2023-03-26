5bdf9250c4f1a654fd407fa8292032613504384d
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2023-03-25 22:18:08 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { useState } from "react";
 
 const App = () => {
   const [temperature, setTemperature] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
 
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display">{temperature}</div>
+        <div className={`temperature-display ${temperatureColor}`}>{temperature}°</div>
       </div>
       <div className="button-container">
         <button onClick={() => setTemperature(temperature + 1)}>+</button>
